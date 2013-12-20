fd572f52516cb3576867c49dd22219d51c52b630
Administrator <Administrator@WIN-MPK917K2TUM>
2013-12-20 14:00:53 +0800

    细节功能完善

---
+++ src/com/wise/wawc/NavigationActivity.java
@@ -0,0 +1,15 @@
+package com.wise.wawc;
+
+import com.baidu.mapapi.BMapManager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NavigationActivity extends Activity {
+	//配置使用百度地图的权限
+	private BMapManager mapMamnager = null;
+//	private MapManagerListener mapManagerListener = null;
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
--- src/com/wise/wawc/SettingCenterActivity.java
+++ src/com/wise/wawc/SettingCenterActivity.java
@@ -3,14 +3,18 @@ package com.wise.wawc;
 import java.util.ArrayList;
 import java.util.List;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.wise.extend.AbstractSpinerAdapter;
 import com.wise.extend.SpinerPopWindow;
 
@@ -32,6 +36,10 @@ public class SettingCenterActivity extends Activity implements OnClickListener, 
 	
 	//显示自定义ListView
 	private SpinerPopWindow mSpinerPopWindow;
+	
+	private AlertDialog dlg = null;  //显示评分对话框
+	private Button gradeCommit = null;  //提交评分
+	private Button gradeCancle = null;   //取消评分
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.setting_center);
@@ -76,16 +84,23 @@ public class SettingCenterActivity extends Activity implements OnClickListener, 
 				ActivityFactory.A.ToHome();
 				break;
 			case R.id.share_have_gift: // 分享有礼
-
+				startActivity(new Intent(SettingCenterActivity.this,ShareActivity.class));
 				break;
 			case R.id.feedback: // 意见反馈
 				startActivity(new Intent(SettingCenterActivity.this,FeedBackActivity.class));
 				break;
 			case R.id.give_us_score: // 给我们评分
-
+				showDialog();
 				break;
 			case R.id.about_appliaction: // 关于我爱我车
 
+				break;
+			case R.id.grade_commit: // 提交评分
+				Toast.makeText(getApplicationContext(), "评分成功", 0).show();
+				dlg.dismiss();
+				break;
+			case R.id.grade_cancle: // 取消评分
+				dlg.dismiss();
 				break;
 			default:
 				return;
@@ -99,7 +114,8 @@ public class SettingCenterActivity extends Activity implements OnClickListener, 
 			mTView.setText(value);
 		}
 	}
-	@Override
+	
+	@Override  //自定义ListView点击监听
 	public void onClick(View v) {
 		switch(v.getId()){
 		case R.id.bt_dropdown:
@@ -107,5 +123,17 @@ public class SettingCenterActivity extends Activity implements OnClickListener, 
 			mSpinerPopWindow.showAsDropDown(mTView);
 			break;
 		}
+	}
+	
+	//点击评分弹出对话框
+	public void showDialog(){
+		LayoutInflater layoutInflater = LayoutInflater.from(SettingCenterActivity.this);
+		View view = layoutInflater.inflate(R.layout.grand_dialog, null);
+		gradeCommit = (Button) view.findViewById(R.id.grade_commit);
+		gradeCancle = (Button) view.findViewById(R.id.grade_cancle);
+		gradeCommit.setOnClickListener(new ClickListener());
+		gradeCancle.setOnClickListener(new ClickListener());
+		dlg = new AlertDialog.Builder(SettingCenterActivity.this).setView(view).setCancelable(true).create();
+		dlg.show();
 	}
 }
