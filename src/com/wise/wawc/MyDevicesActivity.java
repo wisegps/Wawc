037b6bc411cb8e01e624a6a32d47544c9ea9919d
wisegps <wisegps@333>
2013-12-21 18:10:03 +0800

    111

--- src/com/wise/wawc/MyDevicesActivity.java
+++ src/com/wise/wawc/MyDevicesActivity.java
@@ -106,6 +106,7 @@ public class MyDevicesActivity extends Activity{
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			finish();
 			return false;
 		}
 		return super.onKeyDown(keyCode, event);
