c64673b2797e477d57ad8ee0f0b3d83d60d3c621
wisegps <wisegps@333>
2014-01-14 10:15:56 +0800

    缩小图片

--- src/com/wise/pubclas/BlurImage.java
+++ src/com/wise/pubclas/BlurImage.java
@@ -1,6 +1,8 @@
 package com.wise.pubclas;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PorterDuffXfermode;
@@ -38,6 +40,54 @@ public class BlurImage {
         canvas.drawBitmap(bitmap, rect, rect, paint);
         return output;
     }
+    
+    /**
+     * 缩小图片
+     * @param Path 文件sd卡路径
+     * @param reqWidth 缩小的宽度
+     * @param reqHeight 缩小的高度
+     * @return
+     */
+    public static Bitmap decodeSampledBitmapFromResource(String Path, int reqWidth, int reqHeight) {  
+        // 第一次解析将inJustDecodeBounds设置为true，来获取图片大小  
+        final BitmapFactory.Options options = new BitmapFactory.Options();  
+        options.inJustDecodeBounds = true;  
+        //BitmapFactory.decodeResource(res, resId, options);  
+        BitmapFactory.decodeFile(Path, options);
+        // 调用上面定义的方法计算inSampleSize值  
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);  
+        // 使用获取到的inSampleSize值再次解析图片  
+        options.inJustDecodeBounds = false;  
+        return BitmapFactory.decodeFile(Path, options);
+    }
+    
+    /**
+     * 计算缩放尺寸
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public static int calculateInSampleSize(BitmapFactory.Options options,  
+            int reqWidth, int reqHeight) {  
+        //BitmapFactory.decodeFile(pathName, opts)
+        // 源图片的高度和宽度  
+        final int height = options.outHeight;  
+        final int width = options.outWidth;  
+        int inSampleSize = 1;  
+        if (height > reqHeight || width > reqWidth) {  
+            // 计算出实际宽高和目标宽高的比率  
+            final int heightRatio = Math.round((float) height / (float) reqHeight);  
+            final int widthRatio = Math.round((float) width / (float) reqWidth);  
+            // 选择宽和高中最小的比率作为inSampleSize的值，这样可以保证最终图片的宽和高  
+            // 一定都会大于等于目标的宽和高。  
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;  
+        }  
+        return inSampleSize;  
+    }
+    
+    
+    
     
     /** 水平方向模糊度 */
     private static float hRadius = 5;
